b4561f01b5a208ef78f25f609f02cb761d730af4
Andy Obusek <andyo@aweber.com>
2016-06-27 20:45:24 -0400

    Initial Commit

---
+++ LocalReasoningIBOutlets/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LocalReasoningIBOutlets
+//
+//  Created by Andy Obusek on 6/27/16.
+//  Copyright © 2016 Clean Swifter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
